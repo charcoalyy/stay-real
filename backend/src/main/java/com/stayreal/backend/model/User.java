da001e43c5efdc5f3d9c0141848100e55d6c44a7
milo <106563068+charcoalyy@users.noreply.github.com>
2024-01-10 18:02:18 -0500

    fix db config

--- backend/src/main/java/com/stayreal/backend/model/User.java
+++ backend/src/main/java/com/stayreal/backend/model/User.java
@@ -1,9 +1,13 @@
 package com.stayreal.backend.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "app_user")
 public class User {
+    @Id
     private Long id;
     private String username;
     private String email;
